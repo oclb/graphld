f60f4d22a4f48a64563ab0da1d95c027250c3218
Luke Jen O'Connor <loconnor@broadinstitute.org>
2024-12-27 11:19:46 -0500

    Update __init__.py

--- graphld/__init__.py
+++ graphld/__init__.py
@@ -1,7 +1,15 @@
 """LDGM precision module."""
 
-from graphld.io import merge_alleles, merge_snplists
+from graphld.io import load_ldgm, merge_snplists
 from graphld.precision import PrecisionOperator
 from graphld.simulate import Simulate
+from graphld.likelihood import gaussian_likelihood, gaussian_likelihood_gradient, gaussian_likelihood_hessian
 
-__all__ = ['PrecisionOperator', 'merge_snplists', 'merge_alleles', 'Simulate']
+__all__ = ['PrecisionOperator', 
+            'merge_snplists', 
+            'Simulate', 
+            'load_ldgm',
+            'gaussian_likelihood', 
+            'gaussian_likelihood_gradient', 
+            'gaussian_likelihood_hessian',
+            ]
